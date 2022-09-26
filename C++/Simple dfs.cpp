9b45b059fe9107f4a4382f5ea6d1fad15835b1be
DandaIT04 <73043131+DandaIT04@users.noreply.github.com>
2022-09-26 20:41:32 +0800

    Starting simple dfs
    Trying to solve a simple dfs question i made for myself. Might be deleted if idk wtf i be doing

---
+++ C++/Simple dfs.cpp
@@ -0,0 +1,24 @@
+//
+//
+//   4  9 
+//  2    5
+// 1  6  7    
+//  3  8
+//  10 
+//
+//
+//
+
+// Traverse the graph using dfs
+// somehow find shortest path from 1 -> 5 -> 9
+// or if path detects 5 -> 9  combination, stop and print? idk
+
+#include <iostream>;
+
+using namespace std;
+
+// create a something to store number combinations e.g 1->2->4
+
+// create a something to store if that number has been visited -> 0 to 1
+
+// create recursive dfs thing to search tree to find 5 -> 9
