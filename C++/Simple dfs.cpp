a8b0184ba59595ca22ed5c73d4296d3a808a6afd
DandaIT04 <73043131+DandaIT04@users.noreply.github.com>
2022-09-27 00:09:01 +0800

    third update for simple dfs
    I think everything works now, recursively but i think something is wrong DFS wise

--- C++/Simple dfs.cpp
+++ C++/Simple dfs.cpp
@@ -16,45 +16,68 @@
 #include <iostream>;
 #include <bits/stdc++.h>;
 
-vector<int>storeCombinations[9999];
-int visitedNode[10];
-
 using namespace std;
 
 // create a something to store number combinations e.g 1->2->4
 // create a something to store if that number has been visited -> 0 to 1
 // create recursive dfs thing to search tree to find 5 -> 9\
 
-int main(){
+void continueDFS(int i,vector<int>storeCombinations[],int visitedNode[]){
+    visitedNode[i] = 1;
+    cout<<i+1<<" ";
     
-    for int(i=0,i<visitedNode,i++){
+    for (int j:storeCombinations[i]){
+        if(visitedNode[j] == 0){
+            continueDFS(j,storeCombinations,visitedNode);
+        }
+    }
+}
+
+void performDFS(vector<int>storeCombinations[],int numberOfNodes,int visitedNode[]){
+    for (int i=0;i<numberOfNodes;i++){
+        if(visitedNode[i]==0){
+            continueDFS(i,storeCombinations,visitedNode);
+        }
+    }
+}
+
+int main(){
+    vector<int>storeCombinations[10];
+    int visitedNode[10];
+    
+    int numberOfNodes = 10;
+    
+    for (int i=0;i<numberOfNodes;i++){
         visitedNode[i] = 0;
     }
     
-    storeCombinations[1].push_back(2);
-    storeCombinations[2].push_back(1);
+    storeCombinations[0].push_back(1);
+    storeCombinations[1].push_back(0);
+    storeCombinations[0].push_back(2);
+    storeCombinations[2].push_back(0);
     storeCombinations[1].push_back(3);
-    storeCombinations[3].push_back(1);
-    storeCombinations[2].push_back(4);
-    storeCombinations[4].push_back(2);    
-    storeCombinations[3].push_back(10);
-    storeCombinations[10].push_back(3);
-    storeCombinations[2].push_back(6);
-    storeCombinations[6].push_back(2);
-    storeCombinations[3].push_back(6);
-    storeCombinations[6].push_back(3);    
-    storeCombinations[10].push_back(8);
-    storeCombinations[8].push_back(10);
-    storeCombinations[6].push_back(8);
-    storeCombinations[8].push_back(6);
-    storeCombinations[6].push_back(7);
-    storeCombinations[7].push_back(6);
-    storeCombinations[8].push_back(7);
-    storeCombinations[7].push_back(8);
-    storeCombinations[6].push_back(5);
-    storeCombinations[5].push_back(6);
-    storeCombinations[7].push_back(5);
+    storeCombinations[3].push_back(1);    
+    storeCombinations[2].push_back(9);
+    storeCombinations[9].push_back(2);
+    storeCombinations[1].push_back(5);
+    storeCombinations[5].push_back(1);
+    storeCombinations[2].push_back(5);
+    storeCombinations[5].push_back(2);    
+    storeCombinations[9].push_back(7);
+    storeCombinations[7].push_back(9);
     storeCombinations[5].push_back(7);
-    storeCombinations[5].push_back(9);
-    storeCombinations[9].push_back(5);    
+    storeCombinations[7].push_back(5);
+    storeCombinations[5].push_back(6);
+    storeCombinations[6].push_back(5);
+    storeCombinations[7].push_back(6);
+    storeCombinations[6].push_back(7);
+    storeCombinations[5].push_back(4);
+    storeCombinations[4].push_back(5);
+    storeCombinations[6].push_back(4);
+    storeCombinations[4].push_back(6);
+    storeCombinations[4].push_back(8);
+    storeCombinations[8].push_back(4);  
+    
+    performDFS(storeCombinations,numberOfNodes,visitedNode);
+    
 }
