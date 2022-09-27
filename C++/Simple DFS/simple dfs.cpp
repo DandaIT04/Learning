dbebfac3b4c5837115e9d59beabb22693f134bad
DandaIT04 <73043131+DandaIT04@users.noreply.github.com>
2022-09-27 15:19:41 +0800

    27/9/2022 3pm simple dfs done
    Solved self made question by using boolean. If node 5 and 9 detected back to back, print out found and then exit program
    
    Labelled line 30-45 as commentable(//) so that DFS can run fully

--- C++/Simple DFS/simple dfs.cpp
+++ C++/Simple DFS/simple dfs.cpp
@@ -1,5 +1,5 @@
 //
-//
+// Better graph example uploaded as jpeg in folder
 //   4  9 
 //  2    5
 // 1  6  7    
@@ -9,9 +9,8 @@
 //
 //
 
-// Traverse the graph using dfs
-// somehow find shortest path from 1 -> 5 -> 9
-// or if path detects 5 -> 9  combination, stop and print? idk
+// Self made question:
+// Based on jpeg image, use dfs, start at node 1, once node 5 and 9 detected back to back, end program
 
 #include <iostream>;
 #include <bits/stdc++.h>;
@@ -20,23 +19,42 @@ using namespace std;
 
 // create a something to store number combinations e.g 1->2->4
 // create a something to store if that number has been visited -> 0 to 1
-// create recursive dfs thing to search tree to find 5 -> 9\
+// create recursive dfs thing to search tree to find 5 -> 9?
+// Create something that if 5 is detected, the next should be 9 and if true
+// stop the program as 5 -> 9 is found, else revert bool back to false
 
-void continueDFS(int i,vector<int>storeCombinations[],int visitedNode[]){
+void continueDFS(int i,vector<int>storeCombinations[],int visitedNode[],bool firstNodeDetected){
     visitedNode[i] = 1;
     cout<<i+1<<" ";
+
+// Start of code to comment out to run DFS fully +++++++++++++++++++++++++++++++++++++++++++++++++++++   
+    if (firstNodeDetected == true){
+        if (i+1 == 9){
+            cout<<"\n";
+            cout<<"\n 5 -> 9 Found!";
+            exit(0);
+        }
+        else{
+            firstNodeDetected = false;
+        }
+    }
+    
+    if (i+1 == 5){
+        firstNodeDetected = true;
+    }
+// End of code to comment out to run DFS fully +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     
     for (int j:storeCombinations[i]){
         if(visitedNode[j] == 0){
-            continueDFS(j,storeCombinations,visitedNode);
+            continueDFS(j,storeCombinations,visitedNode,firstNodeDetected);
         }
     }
 }
 
-void performDFS(vector<int>storeCombinations[],int numberOfNodes,int visitedNode[]){
+void performDFS(vector<int>storeCombinations[],int numberOfNodes,int visitedNode[],bool firstNodeDetected){
     for (int i=0;i<numberOfNodes;i++){
         if(visitedNode[i]==0){
-            continueDFS(i,storeCombinations,visitedNode);
+            continueDFS(i,storeCombinations,visitedNode,firstNodeDetected);
         }
     }
 }
@@ -46,6 +64,8 @@ int main(){
     int visitedNode[10];
     
     int numberOfNodes = 10;
+    
+    bool firstNodeDetected = false;
     
     for (int i=0;i<numberOfNodes;i++){
         visitedNode[i] = 0;
@@ -64,6 +84,6 @@ int main(){
     storeCombinations[2].push_back(5);
     storeCombinations[2].push_back(0);
     
-    performDFS(storeCombinations,numberOfNodes,visitedNode);
+    performDFS(storeCombinations,numberOfNodes,visitedNode,firstNodeDetected);
     
 }
